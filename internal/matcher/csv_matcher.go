36a32c03c3d2b984bfac4416f5f2220789964684
Hugo Ribeiro <ribeirohugo@outlook.pt>
2023-08-19 16:31:34 +0100

    test: update `matcher` tests

--- internal/matcher/csv_matcher.go
+++ internal/matcher/csv_matcher.go
@@ -79,7 +79,6 @@ func (m *CsvMatcher) open() error {
 // Match is run to find field matches between Csv data file and Csv MD5 encoded data file
 // It will generate a new Csv file with the data file rows that matched with the encoded ones.
 func (m *CsvMatcher) Match() error {
-
 	err := m.open()
 	if err != nil {
 		return err
--- internal/matcher/matcher_test.go
+++ internal/matcher/matcher_test.go
@@ -3,6 +3,9 @@ package matcher
 import (
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 const (
@@ -25,77 +28,93 @@ var outputFile = `202cb962ac59075b964b07152d234b70;123;123
 202cb962ac59075b964b07152d234b70;123;123
 `
 
+func TestCsvMatcher_Match(t *testing.T) {
+	t.Run("should return no error", func(t *testing.T) {
+		t.Run("should match", func(t *testing.T) {
+			// Load Data CSV File
+			dataCsvFile, _ := createTempFile(dataCsvName, dataFile)
+			dataFileName := dataCsvFile.Name()
+			defer os.Remove(dataFileName)
+
+			// Load Encoded Data CSV File
+			encodedCsvFile, _ := createTempFile(encodedCsvName, encodedFile)
+			encodedFileName := encodedCsvFile.Name()
+			defer os.Remove(encodedFileName)
+
+			// Load Encoded Data CSV File
+			outputCsvFile, _ := createTempFile(outputCsvName, "")
+			outputFileName := outputCsvFile.Name()
+			defer os.Remove(outputFileName)
+
+			dataCsv := CsvFile{
+				Delimiter:   delimiter,
+				FilePath:    dataFileName,
+				MatchColumn: 1,
+			}
+
+			encodedCsv := CsvFile{
+				Delimiter:   delimiter,
+				FilePath:    encodedFileName,
+				MatchColumn: 1,
+			}
+
+			matcher := NewCsvMatcher(dataCsv, encodedCsv, outputFileName, encodedColumn)
+
+			err := matcher.Match()
+			assert.Nil(t, err)
+
+			contentBytes, err := os.ReadFile(outputFileName)
+			require.NoError(t, err)
+			assert.Equal(t, outputFile, string(contentBytes))
+
+			dataCsvFile.Close()
+			encodedCsvFile.Close()
+			outputCsvFile.Close()
+		})
+	})
+}
+
 func TestNewCsvMatcher(t *testing.T) {
-	// Load Data CSV File
-	dataCsvFile, _ := createTempFile(dataCsvName, dataFile)
-	dataFileName := dataCsvFile.Name()
-	defer os.Remove(dataFileName)
+	t.Run("should create a CsvMatcher successfully", func(t *testing.T) {
+		const (
+			dataFileName    = "data file name"
+			encodedFileName = "encoded file name"
+			outputFileName  = "output file name"
+		)
 
-	// Load Encoded Data CSV File
-	encodedCsvFile, _ := createTempFile(encodedCsvName, encodedFile)
-	encodedFileName := encodedCsvFile.Name()
-	defer os.Remove(encodedFileName)
-
-	// Load Encoded Data CSV File
-	outputCsvFile, _ := createTempFile(outputCsvName, "")
-	outputFileName := outputCsvFile.Name()
-	defer os.Remove(outputFileName)
-
-	dataCsv := CsvFile{
-		Delimiter:   delimiter,
-		FilePath:    dataFileName,
-		MatchColumn: 1,
-	}
-
-	encodedCsv := CsvFile{
-		Delimiter:   delimiter,
-		FilePath:    encodedFileName,
-		MatchColumn: 1,
-	}
-
-	matcher := NewCsvMatcher(dataCsv, encodedCsv, outputFileName, encodedColumn)
-
-	expected := CsvMatcher{
-		dataCsv: CsvFile{
+		dataCsv := CsvFile{
 			Delimiter:   delimiter,
 			FilePath:    dataFileName,
 			MatchColumn: 1,
-		},
-		encodedCsv: CsvFile{
+		}
+		encodedCsv := CsvFile{
 			Delimiter:   delimiter,
 			FilePath:    encodedFileName,
 			MatchColumn: 1,
-		},
-		writerCsv: CsvWriter{
-			csv: CsvFile{
-				Delimiter: delimiter,
-				FilePath:  outputFileName,
+		}
+
+		expected := CsvMatcher{
+			dataCsv: CsvFile{
+				Delimiter:   delimiter,
+				FilePath:    dataFileName,
+				MatchColumn: 1,
 			},
-		},
-	}
+			encodedCsv: CsvFile{
+				Delimiter:   delimiter,
+				FilePath:    encodedFileName,
+				MatchColumn: 1,
+			},
+			writerCsv: CsvWriter{
+				csv: CsvFile{
+					Delimiter: delimiter,
+					FilePath:  outputFileName,
+				},
+			},
+		}
 
-	if matcher != expected {
-		t.Errorf("CsvMatcher incorrect, \ngot: %v, \nwant: %v.", matcher, expected)
-	}
-
-	err := matcher.Match()
-	if err != nil {
-		t.Errorf("Error returned,\n got: %v,\n want: %v.", err, nil)
-	}
-
-	contentBytes, err := os.ReadFile(outputFileName)
-	content := string(contentBytes)
-	if err != nil {
-		t.Errorf("Error reading file %s", err)
-	}
-
-	if content != outputFile {
-		t.Errorf("Error returned,\n got: %v,\n want: %v.", content, outputFile)
-	}
-
-	dataCsvFile.Close()
-	encodedCsvFile.Close()
-	outputCsvFile.Close()
+		matcher := NewCsvMatcher(dataCsv, encodedCsv, outputFileName, encodedColumn)
+		assert.Equal(t, expected, matcher)
+	})
 }
 
 func createTempFile(name string, content string) (*os.File, error) {
